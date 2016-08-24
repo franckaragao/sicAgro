bb6b37d2b72f8e8c294654b5cfae6fc424e05738
Franck Aragão <fjunior.aragao@gmail.com>
2016-08-24 10:15:00 -0300

    Estruturação

--- src/main/resources/META-INF/sql/insert_admin.sql
+++
@@ -1,5 +0,0 @@
-
-INSERT INTO enderecos VALUES (0, 'Rua Franklin Dantas', '58560-000', 'Ouro Velho', 'casa', 'sn', 'PB', 'Zona Urbana');
-INSERT INTO pessoas (tipo, id, cpf, dta_nascimento, data_registro, nome, telefone, sexo, endereco_FK) VALUES ('Funcionario', 0, '107.836.474-50', '1995-11-07', '2016-08-05 21:36:13.063', 'Franck Aragão', '(83) 99646-3738', 'Masculino', 0);
-INSERT INTO contas (id, email, senha, nome_usuario, funcao_usuario) VALUES (0, 'fjunior.aragao@gmail.com', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'admin', 'ADMIN');
-INSERT INTO funcionarios VALUES (0, 0);

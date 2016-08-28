ac080881db21f2390c44c3d7af3fac360693f950
Franck Aragão <fjunior.aragao@gmail.com>
2016-08-28 11:51:13 -0300

    Exceções por camadas

---
+++ 02 - IMPLEMENTACAO/sicAgro/src/main/java/br/edu/ifpb/sicAgro/exceptions/PersistenceSicAgroException.java
@@ -0,0 +1,11 @@
+package br.edu.ifpb.sicAgro.exceptions;
+
+public class PersistenceSicAgroException extends SicAgroException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public PersistenceSicAgroException(String msg) {
+		super(msg);
+	}
+
+}
---
+++ 02 - IMPLEMENTACAO/sicAgro/src/main/java/br/edu/ifpb/sicAgro/exceptions/ServiceSicAgroException.java
@@ -0,0 +1,11 @@
+package br.edu.ifpb.sicAgro.exceptions;
+
+public class ServiceSicAgroException extends SicAgroException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ServiceSicAgroException(String msg) {
+		super(msg);
+	}
+
+}
